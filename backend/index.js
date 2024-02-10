fb77ef7cc1bd947c2aa8522084b95585ec986b9e
George Parks <60108810+geoge23@users.noreply.github.com>
2024-02-10 04:17:15 +0000

    asdf

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import session from 'express-session';
+import { config } from 'dotenv';
+import client from './utils/postgres.js';
+
+config();
 
 const app = express();
+app.use(session({
+    secret: "bruh", //TODO: fix
+    saveUninitialized: true,
+    cookie: { secure: true }
+}))
 
 app.get('/', (req, res) => {
     res.send('Hello World');
@@ -8,5 +18,5 @@ app.get('/', (req, res) => {
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT || 3000}`);
-    }
+}
 );
